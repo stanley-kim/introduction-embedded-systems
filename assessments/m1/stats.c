2620b5eca0afbeb4628db951ef28a7b73f5902ee
stanley-kim <stanley.will.kim@gmail.com>
2022-01-11 14:02:19 +0000

    Adding final stats.c for C1M1 Assessment

--- assessments/m1/stats.c
+++ assessments/m1/stats.c
@@ -9,13 +9,12 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c
+ * @brief Implementation file for my C-programming code
  *
- * <Add Extended Description Here>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Stanley Kim
+ * @date Jan 11, 2022
  *
  */
 
@@ -37,32 +36,75 @@ void main() {
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
+  print_statistics(test, SIZE);
 }
 
 void print_statistics(unsigned char array[], unsigned int length) {
+  printf("The original Array\n");
+  print_array(array, length);
+  printf("The Median is: %d\n", find_median(array, length));
+  printf("The Mean is: %d\n", find_mean(array, length));
+  printf("The Max is: %d\n", find_max(array, length));
+  printf("The Min is: %d\n", find_min(array, length));
+  sort_array(array, length);
+  printf("The sorted array:\n");
+  print_array(array, length);
 }
 
 void print_array(unsigned char array[], unsigned int length) {
+  for(int i=0; i<length; i++) {
+    printf("%d ", array[i]);
+  }
+  printf("\n");
 }
 
 unsigned char find_median(unsigned char array[], unsigned int length) {
+  unsigned char array_copy[SIZE];
+  for(int i=0; i<length; i++) {
+    array_copy[i] = array[i];
+  }
+  sort_array(array_copy, length);
   return array_copy[(length -1)/2];
+
 }
 
 unsigned char find_min(unsigned char array[], unsigned int length) {
+  unsigned char array_copy[SIZE];
+  for(int i=0; i<length; i++) {
+    array_copy[i] = array[i];
+  }
+  sort_array(array_copy, length);
   return array_copy[length - 1];
 }
 
 unsigned char find_max(unsigned char array[], unsigned int length) {
+  unsigned char array_copy[SIZE];
+  for(int i=0; i<length; i++) {
+    array_copy[i] = array[i];
+  }
+  sort_array(array_copy, length);
   return array_copy[0];
 }
 
 unsigned char find_mean(unsigned char array[], unsigned int length)  {
   unsigned int sum = 0;
+  for(int i=0; i<length;i++) {
+    sum +=  array[i];
+  }
   return (unsigned char)(sum / length);
 }
 
 void sort_array(unsigned char array[], unsigned int length)  {
+  for(int i=(length-1);i>0 ; i-- ) {
+    for(int j=0; j<i; j++) {
+      unsigned int data_swap = 0;
+      if (array[j] < array[j + 1]) {
+        data_swap = array[j + 1];
+        array[j + 1] = array[j];
+        array[j] = data_swap;
+      }
+    }
+  }
 }
 
 
